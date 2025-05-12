49d3c07251e3e0624cd2e2dd1478e95dc8d1e1ce
akshatkanoongo08 <akshatkanoongo08@gmail.com>
2025-05-12 16:15:33 +0530

    project Routes edited

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authmiddleware');
+const { protect } = require('../middleware/authMiddleware');
 const { fieldsUpload } = require('../middleware/uploadMiddleware');
 const {
   getProjects,
